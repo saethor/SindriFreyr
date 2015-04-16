367964aab655ffdb8762694ce207a83fe71e832c
Sæþór Hallgrímsson <saethor94@gmail.com>
2015-04-16 15:16:14 +0000

    UI Modifications and contact form animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$(function() {
+    $('.contact input,textarea').focusin(function() {
+       $(this).parent().find('label').addClass('active');
+    });
+
+    $('.contact input,textarea').focusout(function() {
+
+        var textVal = $(this).val();
+
+        if (textVal === "") {
+            $(this).parent().find('label').removeClass('active');
+        }
+    });
+});
