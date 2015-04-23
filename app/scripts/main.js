404d3e26c4e86041b030e6ba79a24d892026ca97
Sæþór Hallgrímsson <saethor94@gmail.com>
2015-04-23 18:17:25 +0000

    Response social-icon fix, Bio text added and navbar response fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,61 +1,54 @@
+$(document).ready(function() {
 'use strict';
 
-$(document).ready(function() {
 
+// Toggles background color on navbar when user scrolls
+$(window).scroll(function() {
+    var wScroll = $(this).scrollTop();
 
-
-$(function() {
-    $('.contact input,textarea').focusin(function() {
-       $(this).parent().find('label').addClass('active');
-    });
-
-    $('.contact input,textarea').focusout(function() {
-
-        var textVal = $(this).val();
-
-        if (textVal === '') {
-            $(this).parent().find('label').removeClass('active');
-        }
-    });
-
-
-    $('.nav a').click(function() {
-        $('.nav li').removeClass('active');
-        $(this).parent().addClass('active');
-
-    });
-
-    $(window).scroll(function() {
-        var wScroll = $(this).scrollTop();
-        console.log(wScroll);
-
-        if (wScroll > 350) {
-            $('.navbar--main').addClass('scroll');
-        } else {
-            $('.navbar--main').removeClass('scroll');
-
-        }
-    });
-
+    // Determines if jumbotron is still visible. Both on mobile and larger devices
+    if ((wScroll > 350 && window.innerWidth > 770) || (wScroll > 150 && window.innerWidth < 770)) {
+        $('.navbar--main').addClass('scroll');
+    } else {
+        $('.navbar--main').removeClass('scroll');
+    }
 });
 
-    $('.social-icons a').each(function(i) {
-        setTimeout(function(){
-            $('.social-icons a').eq(i).addClass('show');
-        }, 150 * (i + 1));
-    });
+// Toggles active class on clicked links
+$('.nav a').click(function() {
+    $('.nav li').removeClass('active');
+    $(this).parent().addClass('active');
+});
 
-    $('#affix').affix({
-      offset: {
-        top: 100,
-        bottom: function () {
-          return (this.bottom = $('.footer').outerHeight(true));
-        }
-      }
+// Social-icons landing effect
+$('.social-icons a').each(function(i) {
+    setTimeout(function(){
+        $('.social-icons a').eq(i).addClass('show');
+    }, 150 * (i + 1));
 });
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
+
+// Activates tooltip
+$('[data-toggle="tooltip"]').tooltip();
+
+// When user clicks on input label gets class active and moves
+$('.contact input,textarea').focusin(function() {
+   $(this).parent().find('label').addClass('active');
 });
+
+// Checks if input has value. if so it keep the class active. Else it removes it
+$('.contact input,textarea').focusout(function() {
+
+    var textVal = $(this).val();
+
+    if (textVal === '') {
+        $(this).parent().find('label').removeClass('active');
+    }
+});
+
+
+
+
+
 
 
 
